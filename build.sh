0bbb8aeb42d374715403cb5d9596a8d890ba0f99
cozyGalvinism <reallifejunkies@googlemail.com>
2020-09-01 10:58:09 +0200

    Update 'build.sh'

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 rm -rf bin/Release
 rm -rf ItemsDBEditor-*
+
 dotnet publish -r win-x86 -c Release /p:PublishSingleFile=true
-zip -r ./ItemsDBEditor-x86.zip bin/Release/netcoreapp3.1/win-x86/publish
+mkdir ItemsDBEditor-x86
+mv bin/Release/netcoreapp3.1/win-x86/publish/* ./ItemsDBEditor-x86
+zip -r ./ItemsDBEditor-x86.zip ItemsDBEditor-x86
+
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
-zip -r ./ItemsDBEditor-x64.zip bin/Release/netcoreapp3.1/win-x64/publish
+mkdir ItemsDBEditor-x64
+mv bin/Release/netcoreapp3.1/win-x64/publish/* ./ItemsDBEditor-x64
+zip -r ./ItemsDBEditor-x64.zip ItemsDBEditor-x64
+
 dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
-zip -r ./ItemsDBEditor-OSX.zip bin/Release/netcoreapp3.1/osx-x64/publish
+mkdir ItemsDBEditor-OSX
+mv bin/Release/netcoreapp3.1/osx-x64/publish/* ./ItemsDBEditor-OSX
+zip -r ./ItemsDBEditor-OSX.zip ItemsDBEditor-OSX
+
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
-zip -r ./ItemsDBEditor-Linux-x64.zip bin/Release/netcoreapp3.1/linux-x64/publish
+mkdir ItemsDBEditor-Linux-x64
+mv bin/Release/netcoreapp3.1/linux-x64/publish/* ./ItemsDBEditor-Linux-x64
+zip -r ./ItemsDBEditor-Linux-x64.zip ItemsDBEditor-Linux-x64
